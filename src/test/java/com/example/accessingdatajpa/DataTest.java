4f03a5d52c863713dabee9c40e1b77a0311c562c
Thomas Fischer <tf@media-nox.de>
2021-05-02 15:02:08 +0200

    added datajpatest

---
+++ src/test/java/com/example/accessingdatajpa/DataTest.java
@@ -0,0 +1,26 @@
+package com.example.accessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class DataTest {
+
+	@Autowired
+	CustomerSearchRepository customerSearchRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Test
+	void count_entries() {
+		final long count = customerRepository.count();
+		assertThat(count).isEqualTo(3);
+	}
+}
